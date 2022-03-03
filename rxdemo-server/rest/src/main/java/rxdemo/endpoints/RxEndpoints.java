59b57eed4ab02f71c9905bfef3e57d0c1b71a924
Jason Leeneman <jason.leeneman@proudnerds.com>
2022-03-03 13:53:52 +0100

    New code change

--- rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
+++ rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
@@ -11,7 +11,7 @@ import spark.Response;
 import static spark.Spark.*;
 
 // change to be deployed
-// Trigger here
+// Trigger here again
 // Changed again
 public class RxEndpoints {
    public static void initalizeEndpoints() {
